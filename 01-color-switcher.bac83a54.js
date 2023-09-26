18167f558cf2383f0c750bff91fa8aebd97da623
Razvan49 <128290757+Razvan49@users.noreply.github.com>
2023-09-26 20:48:54 +0000

    Deploying to gh-pages from @ Razvan49/goit-js-hw-09@7b9f80da1088db01dfd6c5e46237afd84125aa31 🚀

---
+++ 01-color-switcher.bac83a54.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let n=null;t.addEventListener("click",(()=>{n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(n),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.bac83a54.js.map
